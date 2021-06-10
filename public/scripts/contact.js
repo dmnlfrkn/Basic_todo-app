79935d96b8652e3f879c1a65d3dd980b24c56fce
Furkan DUMANLI <66632576+dmnlfrkn@users.noreply.github.com>
2021-06-10 16:57:09 +0300

    Add files via upload

---
+++ public/scripts/contact.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  var btn = document.getElementById("submit-buton");
+  $(btn).click(function () {
+    alert("Your post has been received.");
+  });
+});
---
+++ public/scripts/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    var loader = document.getElementsByClassName("loader");
+    var btn = document.getElementById("submit-buton");
+
+    $(loader).fadeOut(0);
+
+    $(btn).click(function() {
+        if(username.value=="admin" && password.value =="1234"){ 
+            $(loader).fadeIn(3000);
+            setTimeout(() => {
+                    document.getElementById("myform").submit();
+            },3000);
+        }
+        else{
+            alert("Please Enter valid username and password");
+        }
+    });
+    
+    
+
+
+}); 
