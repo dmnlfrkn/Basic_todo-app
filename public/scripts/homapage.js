fe8433c9c00cac6d0f67d812b04dbd572c9b1245
Furkan DUMANLI <66632576+dmnlfrkn@users.noreply.github.com>
2021-06-12 22:36:17 +0300

    Update homapage.js

--- public/scripts/homapage.js
+++ public/scripts/homapage.js
@@ -1,203 +1,186 @@
+let task_count = 1;
+let project_count = 2;
+let projects = [];
 
-!(function (evt) {
-    var temp = {};
-    function n(r) {
-        if (temp[r]) return temp[r].exports;
-        var l = (temp[r] = { i: r, l: !1, exports: {} });
-        return evt[r].call(l.exports, l, l.exports, n), (l.l = !0), l.exports;
-    }
-    (n.m = evt),
-        (n.c = temp),
-        (n.d = function (e, t, r) {
-            n.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: r });
-        }),
-        (n.r = function (e) {
-            "undefined" != typeof Symbol &&
-                Symbol.toStringTag &&
-                Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
-                Object.defineProperty(e, "__esModule", { value: !0 });
-        }),
-        (n.t = function (e, t) {
-            if ((1 & t && (e = n(e)), 8 & t)) return e;
-            if (4 & t && "object" == typeof e && e && e.__esModule) return e;
-            var r = Object.create(null);
-            if (
-                (n.r(r),
-                    Object.defineProperty(r, "default", { enumerable: !0, value: e }),
-                    2 & t && "string" != typeof e)
-            )
-                for (var l in e)
-                    n.d(
-                        r,
-                        l,
-                        function (t) {
-                            return e[t];
-                        }.bind(null, l)
-                    );
-            return r;
-        }),
-        (n.n = function (e) {
-            var t =
-                e && e.__esModule
-                    ? function () {
-                        return e.default;
-                    }
-                    : function () {
-                        return e;
-                    };
-            return n.d(t, "a", t), t;
-        }),
-        (n.o = function (e, t) {
-            return Object.prototype.hasOwnProperty.call(e, t);
-        }),
-        (n.p = ""),
-        n((n.s = 0));
-})([
-    function (e, t, n) {
-        "use strict";
-        function r(e, t, n, r) {
-            const l = document.createElement(e);
-            return (
-                t && (l.id = t),
-                n && n.forEach((e) => l.classList.add(e)),
-                r && (l.innerText = r),
-                l
-            );
-        }
-        function l() {
-            return Math.random().toString().split(".").join("");
-        }
-        n.r(t);
-        const d = document.querySelector("#folders .card");
-        class i {
-            constructor(e) {
-                (this.title = e), (this.id = l()), (this.tasks = []);
-            }
-            render() {
-                const e = r("div", this.id, ["folder"], null),
-                    t = r("p", null, null, this.title),
-                    n = r("img", null, ["delete"], null);
-                (n.src = "./img/delete.svg"),
-                    n.addEventListener("click", () => {
-                        this.delete();
-                    }),
-                    e.appendChild(t),
-                    e.appendChild(n),
-                    d.appendChild(e),
-                    e.addEventListener("click", () => {
-                        (a = this),
-                            (a.active = !1),
-                            this.renderTasks(),
-                            document
-                                .querySelectorAll(".folder")
-                                .forEach((e) => (e.classList.value = "folder")),
-                            document
-                                .querySelectorAll(".delete")
-                                .forEach((e) => (e.src = "./img/delete.svg")),
-                            e.classList.value.includes("active") ||
-                            (e.classList.add("active"), (n.src = "./img/delete-white.svg"));
-                    });
-            }
-            delete() {
-                (s = s.filter((e) => e.id !== this.id)), i.renderFolders();
-            }
-            renderTasks() {
-                (document.querySelector("#task-container").innerHTML = ""),
-                    this.tasks.forEach((e) => e.render());
-            }
-            static renderFolders() {
-                (d.innerHTML = ""), s.forEach((e) => e.render());
-            }
-            static setCurrentFolder(e) {
-                a = e;
-            }
-        }
-        let s = [new i("Test Folder")],
-            a = s[0];
-        const o = document.querySelector("#task-container");
-        class u {
-            constructor(e, t, n) {
-                (this.name = e), (this.date = t), (this.priority = n), (this.id = l());
-            }
-            render() {
-                const e = r("div", this.id, ["task"], null),
-                    t = r("div", null, ["task-name"], null),
-                    n = r("input", null, ["checkbox"], null);
-                n.type = "checkbox";
-                const l = r("p", null, null, this.name),
-                    d = r("div", null, ["task-info"], null),
-                    i = r("p", null, null, this.date),
-                    s = r("div", null, ["badge"], this.priority),
-                    a = r("img", null, ["delete"], null);
-                a.src = "./img/delete.svg";
-                const u = r("img", null, ["edit"], null);
-                (u.src = "./img/edit.svg"),
-                    u.addEventListener("click", () => {
-                        this.edit();
-                    }),
-                    t.appendChild(n),
-                    t.appendChild(l),
-                    d.appendChild(i),
-                    d.appendChild(s),
-                    d.appendChild(a),
-                    d.appendChild(u),
-                    e.appendChild(t),
-                    e.appendChild(d),
-                    o.appendChild(e),
-                    n.addEventListener("change", () => {
-                        l.classList.toggle("done");
-                    }),
-                    "urgent" === this.priority && s.classList.add("urgent"),
-                    a.addEventListener("click", () => {
-                        this.delete();
-                    });
-            }
-            delete() {
-                (a.tasks = a.tasks.filter((e) => e.id !== this.id)), a.renderTasks();
-            }
-            edit() {
-                const e = document.getElementById("myModal");
-                e.style.display = "flex";
-                document
-                    .getElementById("form-task-edit")
-                    .addEventListener("submit", (t) => {
-                        t.preventDefault();
-                        const n = document.getElementById("task-name-edit");
-                        if ("" === n.value) return void alert("The task must have a name");
-                        
-                        
-                        (this.name = n.value),
-                            (this.date = r.value),
-                            (this.priority = l.value),
-                            (e.style.display = "none"),
-                            a.renderTasks();
-                    });
-            }
-        }
-        document.getElementById("form-folder").addEventListener("submit", (e) => {
-            e.preventDefault();
-            const t = document.getElementById("folder-name");
-            if ("" === t.value) return void alert("The folder must have a name");
-            const n = new i(t.value);
-            s.push(n), (t.value = ""), i.renderFolders(), i.setCurrentFolder(n);
-            let r = document.querySelectorAll(".folder");
-            r[r.length - 1].classList.add("active"),
-                (r[r.length - 1].lastChild.src = "./img/delete-white.svg");
+  $.getJSON("datas.json", function (data) {
+      for (let i = 0; i < 3; i++) {
+        projects.push({
+          project_name: data[i].project_name,
+          task_name: data[i].task_name,
+          task_description: data[i].task_description,
+          task_id:"task"+i,
+          id: "project" + 1
         });
-        document.getElementById("form-task").addEventListener("submit", (e) => {
-            e.preventDefault();
-            const t = document.getElementById("task-name");
-            if ("" === t.value) return void alert("The task must have a name");
-            
-            
-            const l = new u(t.value, n.value, r.value);
-            a.tasks.push(l), l.render();
-        }),
-            i.renderFolders();
-        
-    },
-]);
+      }
+  });
 
 
-	
-	
+function showCreate() {
+  document.getElementById("prjct-crt-frm").style.display = "block";
+}
+function closeCrt() {
+  document.getElementById("prjct-crt-frm").style.display = "none";
+}
+
+function showTask() {
+  document.getElementById("task-create-form").style.display = "block";
+}
+function closeTask() {
+  document.getElementById("task-create-form").style.display = "none";
+}
+function create_prjct_mobile() {
+  let p_panel = document.getElementById("project-panel");
+  let t_panel = document.getElementById("task-panel");
+  if (p_panel.style.display === "block") {
+    p_panel.style.display = "none";
+    t_panel.style.display = "block";
+  } else {
+    t_panel.style.display = "none";
+    p_panel.style.width = "100%";
+    p_panel.style.display = "block";
+  }
+}
+
+function addProject() {
+  let name = document.getElementById("prjct-name").value;
+
+  if (name == "") {
+    alert("Please write a valid name!");
+  } else {
+
+    document.getElementById("prjct-crt-frm").style.display = "none";
+    let project = document.getElementById("projects");
+   
+    project.innerHTML +=
+      "<div id='project" + project_count + "'class='project' onclick='selectProject(this)' ><p >" + name + "</p>" +
+      "<button class='edit-btn' onclick='editProject(this)'>" + "Edit" + "</button>" +
+      "<button class='delete-btn' id='jq_delete' onclick='deleteProject(this)'>" + "Delete" + "</button>" +
+      "</div>";
+    document.getElementById("prjct-name").value = "";
+    project_count++;
+  }
+}
+
+function selectProject(this_p) {
+
+  let name = this_p.children[0].innerHTML;
+  if (document.getElementById("project-name") != null) {
+    let pName = document.getElementById("project-name");
+    pName.innerHTML = "<p id='ppp'>" + name + "</p>";
+  }
+  document.getElementById("tasks").innerHTML = "";
+  showTasks(this_p);
+}
+
+function showTasks(params) {
+  document.getElementById("task-panel").style.display = "block ";
+  let task = document.getElementById("tasks");
+  let result=[];
+  let objectLength = Object.keys(projects).length;
+  for (let i = 0; i < objectLength; i++) {
+      if(projects[i].id==params.id && projects[i].task_name!="" ){
+        result.push(projects[i]);
+      }
+  }
+
+  for (let index = 0; index < result.length; index++) {
+      task.innerHTML += "<div class='task' id='" + result[index].task_id + "'>" +
+        "<h1>" + result[index].task_name + "</h1>" +
+        "<p>" + result[index].task_description + "</p>" +
+        "<button class='edit-btn' onclick='editTask(this)'>" + "Edit" + "</button>" +
+        "<button class='delete-btn' onclick='deleteTask(this)'>" + "Delete" + "</button>" +
+        "</div>";
+
+  }
+}
+
+function addTask() {
+  let name = document.getElementById("task-name").value;
+  let description = document.getElementById("description").value;
+  if (name == "" && description == "") {
+    alert("Please write a valid  Task name and description!");
+  } else {
+    let p_name =document.getElementById("ppp").innerHTML;
+    projects.push({
+      project_name: p_name,
+      task_name: name,
+      task_description: description,
+      task_id:"task"+task_count,
+      id: "project" + project_count
+    });
+
+    document.getElementById("task-create-form").style.display = "none";
+    let task = document.getElementById("tasks");
+
+    task.innerHTML += "<div class='task' id='" + "task" + task_count + "'>" +
+      "<h1>" + name + "</h1>" +
+      "<p>" + description + "</p>" +
+      "<button class='edit-btn' onclick='editTask(this)'>" + "Edit" + "</button>" +
+      "<button class='delete-btn' onclick='deleteTask(this)'>" + "Delete" + "</button>" +
+      "</div>";
+
+    document.getElementById("task-name").value = "";
+    document.getElementById("description").value = "";
+
+    task_count++;
+  }
+}
+function editTask(this_t) {
+  /*    tasl name part  */
+  var default_pName = document.getElementById(this_t.parentElement.id);
+  var txt;
+  var edit = prompt("Please enter new projcet name:", default_pName.children[0].innerHTML);
+  if (edit == null || edit == "") {
+    txt = default_pName.children[0].innerHTML;
+  } else {
+    txt = edit;
+  }
+  default_pName.children[0].innerHTML = txt;
+
+  /*    description part  */
+  var dsc;
+  var edit = prompt("Please enter new projcet name:", default_pName.children[1].innerHTML);
+  if (edit == null || edit == "") {
+    dsc = default_pName.children[0].innerHTML;
+  } else {
+    dsc = edit;
+  }
+  default_pName.children[1].innerHTML = dsc;
+}
+function editProject(this_p) {
+
+  var default_pName = document.getElementById(this_p.parentElement.id);
+  var txt;
+  var edit = prompt("Please enter new projcet name:", default_pName.children[0].innerHTML);
+  if (edit == null || edit == "") {
+    txt = default_pName.children[0].innerHTML;
+  } else {
+    txt = edit;
+  }
+  default_pName.children[0].innerHTML = txt;
+}
+
+function deleteTask(params) {
+  params.parentElement.remove();
+}
+function deleteProject(params) {
+
+  
+
+  let task =document.getElementById("tasks");
+
+  for(var i=0;i<task.children.length;i++){
+    var index = projects.findIndex(function (o) {
+      return o.id === params.parentElement.id;
+    });
+    if (index !== -1) projects.splice(index, 1);
+    params.parentElement.remove();
+  }
+  document.getElementById("task-panel").style.display = "none ";
+}
+
+
+
+
+
+
